f506a235e9917d78d81555887fce8a03405c9b24
griffinrobotics11666 <griffinrobotics11666@gmail.com>
2021-11-28 16:16:53 -0700

    - bumped joos version  - installed EasyOpenCV

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTest.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvWebcam;
+
+@TeleOp(name = "Camera Test")
+public class CameraTest extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+        OpenCvWebcam webcam = OpenCvCameraFactory.getInstance().createWebcam(webcamName);
+
+        telemetry.addData("", "loading camera...");
+        telemetry.update();
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened()
+            {
+                dashboard.startCameraStream(webcam, 60);
+                telemetry.addData("", "camera opened!");
+                telemetry.update();
+            }
+
+            @Override
+            public void onError(int errorCode) {
+                telemetry.addData("", "camera failed to open.");
+                telemetry.update();
+            }
+        });
+    }
+}
